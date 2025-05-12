2acae0cc82ef154d0cebf11861397035f4b223a0
yivanovaa <yanitsaiv@icloud.com>
2025-05-12 16:11:34 -0700

    local setup

--- alembic/env.py
+++ alembic/env.py
@@ -11,7 +11,7 @@ config = context.config
 config.set_main_option(
     "sqlalchemy.url",
     os.getenv(
-        "POSTGRES_URI", "postgresql+psycopg://myuser:mypassword@localhost/mydatabase"
+        "POSTGRES_URI", "postgresql+psycopg://myuser:mypassword2@localhost:5433/mydatabase"
     ),
 )
 
